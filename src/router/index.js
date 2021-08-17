6fb2425ac8ffaa380721ca917ea6985c939177e5
James <james.ryan.mcguire@gmail.com>
2021-08-17 16:26:11 -0400

    Adds and imports Signup.vue page with route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
 import WorkoutsIndex from "../views/WorkoutsIndex";
 import WorkoutsNew from "../views/WorkoutsNew";
 import WorkoutsShow from "../views/WorkoutsShow";
@@ -22,6 +23,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: Signup,
   },
   {
     path: "/workouts",
