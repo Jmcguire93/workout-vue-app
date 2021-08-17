4a0b279e1b3996ff892ef438f3c6f3966c037a46
James <james.ryan.mcguire@gmail.com>
2021-08-17 13:21:41 -0400

    Adds data function, get request, route and link

---
+++ src/views/WorkoutsIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="workouts-index">
+    <h1>{{ message }}</h1>
+    <div v-for="workout in workouts" :key="workout.id">
+      <h2>Name: {{ workout.name }}</h2>
+      <p>Description: {{ workout.description }}</p>
+      <!-- <img v-bind:src="workout.image" alt="workout.name" /> -->
+      <p><router-link v-bind:to="`/workouts/${workout.id}`">Link to workout</router-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Workouts",
+      workouts: [],
+      newWorkoutParams: {},
+      currentWorkout: {},
+    };
+  },
+  created: function () {
+    this.indexWorkouts();
+  },
+  methods: {
+    indexWorkouts: function () {
+      axios.get("http://localhost:3000/workouts").then((response) => {
+        this.workouts = response.data;
+        console.log("All workouts:", this.workouts);
+      });
+    },
+  },
+};
+</script>
