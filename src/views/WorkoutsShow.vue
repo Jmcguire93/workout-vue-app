7850347833a4b334f182df90abbd93c153fcce02
James <james.ryan.mcguire@gmail.com>
2021-08-22 18:33:09 -0400

    Changed current_user_id to user_id in data function, and added edit button to each exercise within workout object

--- src/views/WorkoutsShow.vue
+++ src/views/WorkoutsShow.vue
@@ -10,10 +10,34 @@
         <p>Sets: {{ workout_exercises[index].sets }}</p>
         <p>Reps: {{ workout_exercises[index].reps }}</p>
         <p>Weight: {{ workout_exercises[index].weight }}</p>
+        <p>Notes: {{ workout_exercises[index].notes }}</p>
 
-        <!-- <img v-bind:src="exercise.image" alt="workout.name" /> -->
-        <!-- </div> -->
+        <li v-if="$parent.getUserId() == user_id">
+          <button v-on:click="openModal()">Edit Exercise</button>
+        </li>
       </div>
+      <dialog id="workout-exercises">
+        <form method="dialog">
+          <p>
+            Sets:
+            <input type="text" v-model="newWorkoutExerciseParams.sets" />
+          </p>
+          <p>
+            Reps:
+            <input type="text" v-model="newWorkoutExerciseParams.reps" />
+          </p>
+          <p>
+            Weight:
+            <input type="text" v-model="newWorkoutExerciseParams.weight" />
+          </p>
+          <p>
+            Notes:
+            <input type="text" v-model="newWorkoutExerciseParams.notes" />
+          </p>
+          <button>Close</button>
+          <button v-on:click="updateWorkoutExercise()">Update Exercise</button>
+        </form>
+      </dialog>
 
       <li v-if="$parent.getUserId() == workout.user_id">
         <router-link v-bind:to="`/workouts/${workout.id}/edit`"><button>Edit workout</button></router-link>
@@ -33,8 +57,10 @@ export default {
       workout: {},
       exercises: [],
       user: {},
-      current_user_id: localStorage.getItem("user_id"),
+      user_id: localStorage.getItem("user_id"),
       workout_exercises: [],
+      newWorkoutExerciseParams: {},
+      currentWorkoutExercise: {},
     };
   },
   created: function () {
@@ -43,13 +69,24 @@ export default {
       this.exercises = response.data.exercises;
       this.user = response.data.user;
       this.workout_exercises = response.data.workout_exercises;
-      console.log("Current user id", this.current_user_id);
+      console.log("Current user id", this.user_id);
       console.log("This workout", this.workout.user_id);
       console.log("Exercises", this.exercises);
       console.log("Users", this.user);
       console.log("This workout", this.workout);
       console.log("Jointable", response.data.workout_exercises);
     });
+  },
+  methods: {
+    openModal: function () {
+      document.querySelector("#workout-exercises").showModal();
+    },
+    updateWorkoutExercise: function () {
+      axios.patch(`/workout_exercises/${this.$route.params.id}`, this.newWorkoutExerciseParams).then((response) => {
+        console.log(response.data);
+        this.$router.push(`/workouts/${this.$route.params.id}`);
+      });
+    },
   },
 };
 </script>
