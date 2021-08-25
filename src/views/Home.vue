edf33bccdcc699e57c53f8e33afbfec3af2235a5
James <james.ryan.mcguire@gmail.com>
2021-08-25 12:50:00 -0400

    Switched icon classes for exerises and create and added subheadings

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,25 +32,25 @@
                   <a href="/workouts" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
                   <header class="second icon solid fa-user">
                     <h3>Workouts</h3>
-                    <p>And a subtitle</p>
+                    <p>View user created workouts</p>
                   </header>
                 </section>
               </div>
               <div class="col-4 col-12-medium">
                 <section>
                   <a href="/exercises" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-                  <header class="second icon solid fa-cog">
+                  <header class="second icon solid fa-chart-bar">
                     <h3>Exercises</h3>
-                    <p>And another subtitle</p>
+                    <p>Explore exercises</p>
                   </header>
                 </section>
               </div>
               <div class="col-4 col-12-medium">
                 <section>
                   <a href="/workouts/new" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                  <header class="second icon solid fa-chart-bar">
+                  <header class="second icon solid fa-cog">
                     <h3>Create Workout</h3>
-                    <p>And yes, a subtitle</p>
+                    <p>Create your own!</p>
                   </header>
                 </section>
               </div>
